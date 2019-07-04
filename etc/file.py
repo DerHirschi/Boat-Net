3945c445b6d4d550fb48c0e734db9d042b09a284
DerHirschi <hirschi@saw-emusic.de>
2019-07-04 15:44:37 +0200

    00 Projekt erstellt aus AntennenRotator_dev

---
+++ etc/file.py
@@ -0,0 +1,107 @@
+# Author DerHirschi
+import os
+
+from etc.var import count_filename, change_file_ext
+from etc.log import log
+from etc.crypt import enc, dec
+
+
+# 1 Daten ( egal welcher daten typ)
+# 2 Dateiname
+# 3 (opt)Neue Datei erstell wenn Datei schon existiert - default True
+# 4 (opt)Pfad - default Projekt Rootverzeichniss
+# 5 (opt)Option Datum oder Zeit im Dateinamen + Count
+#   'count'/'date'/'time'
+# return filename string -> return '' if except
+def write(data, f_ile, new_file=True, path='', opt='count'):
+    def _add_nl(st_r):
+        if type(st_r) != str:
+            st_r = str(st_r)
+        if bool(st_r.find('\n') + 1):
+            return st_r
+        else:
+            return st_r + '\n'
+
+    _f_name = path + f_ile
+    if new_file:
+        _f_name = count_filename(_f_name, opt)
+
+    try:
+        _f = open(_f_name, 'a')
+
+        if type(data) == str:
+            _f.write(_add_nl(data))
+        elif type(data) == list or type(data) == tuple:
+            for i in range(len(data)):
+                _f.write(_add_nl(data[i]))
+        elif type(data) != dict:
+            _f.write(_add_nl(data))
+
+        _f.close()
+        return _f_name
+    except:
+        log('{} - write() Writefile'.format(_f_name), 2)
+        return ''
+
+
+def try_file(f_name, opt):
+    try:
+        return open(f_name, opt)
+    except:
+        log('{} - try_file() opt: {}'.format(f_name, opt), 2)
+        return False
+
+
+# 1 Path + Filename
+# 2 Key (Password)
+# 3 (opt) Output Filename extension.. examples: ('cry'/'no'/'zip' ..) - default *.enc
+# 4 (opt) True = Insert IV in File
+# return IV, Otuputfilename
+def enc_file(f_ile, key, f_ext='enc', iv_add=True):
+    _wf_name = count_filename(change_file_ext(f_ile, f_ext))
+    if os.path.exists(f_ile) and not os.path.exists(_wf_name):
+        _rf = try_file(f_ile, 'r')
+        _wf = try_file(_wf_name, 'w')
+        if _rf and _wf:
+            _res = enc(key, _rf.read())
+            _rf.close()
+            if iv_add:
+                _wf.write(_res[1] + _res[0])
+            else:
+                _wf.write(_res[0])
+            _wf.close()
+            return _res[1], _wf_name
+
+# 1 Path + Filename
+# 2 Key (Password)
+# 3 (opt) Output Filename extension.. examples: ('cry'/'no'/'zip' ..) - default *.dec
+# 4 (opt) IV .. if leave '' the fnc take the IV from File
+# return Otuputfilename
+def dec_file(f_ile, key, f_ext='dec', iv=''):
+    _wf_name = count_filename(change_file_ext(f_ile, f_ext))
+    if os.path.exists(f_ile) and not os.path.exists(_wf_name):
+        _rf = try_file(f_ile, 'r')
+        _wf = try_file(_wf_name, 'w')
+        if _rf and _wf:
+            _tmp = _rf.read()
+            _rf.close()
+            if iv == '':
+                _wf.write(dec(key, _tmp[16:], _tmp[:16]))
+            else:
+                _wf.write(dec(key, _tmp, iv))
+
+            _wf.close()
+            return _wf_name
+        else:
+            return ''
+    else:
+        return ''
+
+
+# Test it !!!
+
+# enc_file('test.txt', 'asd4tgdhGFSdhkdsas34shWshAsh4')
+# write('jeahhh','test.txt', True, '',)
+# write('jeahhh','test.txt', True, '', 'date')
+# write('jeahhh','test.txt', True, '', 'time')
+# write('jeahhh','test.txt', True, '', 'time')
---
+++ etc/var.py
@@ -0,0 +1,171 @@
+import os
+import time
+
+
+def array2string(data, cut_flag=',', cut_last=True):
+    _temp = ''
+    for i in range(len(data)):
+        if type(data[i]) != str:
+            _temp = _temp + str(data[i]) + cut_flag
+        else:
+            _temp = _temp + data[i] + cut_flag
+    if cut_last:
+        return _temp[:(len(_temp) - 1)]
+    else:
+        return _temp[:(len(_temp))]
+
+
+def string2array(data, cut_flag=' ', cut_blank=True, conv2int=True):
+    _data = data.split(cut_flag)
+    _temp = []
+    for i in range(len(_data)):
+        if _data[i] == '' and cut_blank:
+            pass
+        else:
+            try:
+                if conv2int:
+                    _temp = _temp + [int(_data[i])]
+                else:
+                    _temp = _temp + [_data[i]]
+            except:
+                _temp = _temp + [_data[i]]
+    return _temp
+
+
+# Quelle: https://stackoverflow.com/questions/931092/reverse-a-string-in-python
+def rev_str(a_string):
+    return a_string[::-1]
+
+
+def ck_str(_data):
+    if type(_data) != str:
+        _data = str(_data)
+    return _data
+
+
+def ck_list_ele_type(_data, _type):
+    if not type(_data) == list:
+        return 0
+    else:
+        for _i in _data:
+            if type(_i) != _type:
+                return 0
+        return 1
+
+
+def get_time(opt='all', string=False):
+    def _monat(lst, trig):
+        if not trig:
+            return lst
+        else:
+            return {
+                'Jan': 1,
+                'Feb': 2,
+                'Mar': 3,
+                'Apr': 4,
+                'May': 5,
+                'Jun': 6,
+                'Jul': 7,
+                'Aug': 8,
+                'Sep': 9,
+                'Oct': 10,
+                'Nov': 11,
+                'Dec': 12,
+            }[lst]
+
+    def _conv_all():
+        if string:
+            return array2string(string2array(time.ctime(), ' '), '-')
+        else:
+            return string2array(time.ctime(), ' ')
+
+    _day_string = {
+        True: 0,
+        False: 2,
+    }
+
+    return {
+        # 'all': string2array(time.ctime(), ' '),
+        'all': _conv_all(),
+        'year': string2array(time.ctime(), ' ', True, not string)[4],
+        'mon': _monat(string2array(time.ctime(), ' ')[1], not string),
+        'day': string2array(time.ctime(), ' ')[_day_string[string]],
+        'time': string2array(time.ctime(), ' ')[3],
+        'h': string2array(str(string2array(time.ctime(), ' ')[3]), ':', True, not string)[0],
+        'min': string2array(str(string2array(time.ctime(), ' ')[3]), ':', True, not string)[1],
+        'sek': string2array(str(string2array(time.ctime(), ' ')[3]), ':', True, not string)[2],
+    }[opt]
+
+
+# Fuegt an Dateinamenstring Datum oder Zeit an
+# 1 Dateiname
+# 2 Option ('date'/'time')
+# 3 Cut Flag -> c_f='_' -> 2017_9_1 or c_f='!' -> 2017!9!1
+# !!!! Prueft nicht ob Datei schon existiert !!!! fuer log Funktion
+
+def date2filename(f_name, time_form='date', cut_flag='_'):
+    _i = f_name.find('.')
+    _temp = f_name[_i:]
+    return f_name[:_i] + '_' + build_date_st(time_form, cut_flag) + _temp
+
+# 1 Option 'date'/'time'/'all'
+# 2 Cut_Flag -> c_f='_' -> 2017_9_1 or c_f='!' -> 2017!9!1
+
+
+def build_date_st(t_f, c_f):
+    if t_f == 'all':
+        return build_date_st('date', c_f) + c_f + build_date_st('time', c_f)
+    else:
+        return {
+            'date': str(get_time('year')) + c_f + str(get_time('mon')) + c_f + str(get_time('day')),
+            'time': str(get_time('h')) + c_f + str(get_time('min')) + c_f + str(get_time('sek')),
+        }[t_f]
+
+# Prueft ob Datei schon existiert.
+# Wenn ja wird ein count (_1 / _2 ...) an den Dateinamen angehaengt
+# und der neue Dateinnamen String zurueck gegeben
+# 1 Dateiname
+# 2 Option ( 'count'/'date'/'time' ) -> date & time fuegt Datum oder Zeit vor dem Count ein
+
+
+def count_filename(f_name, opt='count'):
+    def _count_st(_f_name, _end):
+        if not os.path.exists(_f_name + _end):
+            return _f_name + _end
+        else:
+            _n = 1
+            _l = len(_f_name)
+            while os.path.exists(_f_name + _end):
+                _f_name = _f_name[:_l] + '_{}'.format(_n)
+                _n += 1
+            return _f_name + _end
+
+    _i = (f_name.find('.'))
+    _e = f_name[_i:]
+    _n = f_name[:_i]
+
+    if opt == 'count':
+        return _count_st(_n, _e)
+    else:
+        return {
+            'date': _count_st(_n + build_date_st(opt, '-'), _e),
+            'time': _count_st(_n + build_date_st(opt, '-'), _e)
+        }[opt]
+
+
+# Andert Dateiendung
+def change_file_ext(f_name, ext):
+    return f_name[:(f_name.find('.'))] + '.{}'.format(ext)
+
+
+# Overflow a int var
+def overflow_int(_var, _bit=8):
+    _bit = (2**_bit) - 1
+    if _var > _bit:
+        return (_var - _bit), True
+    else:
+        return _var, False
+
+
+if __name__ == '__main__':
+    print(overflow_int(256))
